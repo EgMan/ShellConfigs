15dedf8f028cad52fe537bb88e27e08cdf05c5c4
EgMan <antonio95woods@gmail.com>
2018-06-08 12:51:43 -0500

    Refactored to use functions for script paths

--- commonrc.sh
+++ commonrc.sh
@@ -1,20 +1,21 @@
 #Local
-self=`realpath $BASH_SOURCE`
-path=`dirname $self`
-name=`basename $self`
-
-#Aliases
-alias commonrc="vim $self"
-alias refresh="source $self"
-alias bookmarks="vim $path/bookmarks"
-alias vimrc="source $path/vimrc.vim"
-
-#Sources
-source ${path}/cernrc.sh
+hist_size=1000000
 
 #Functions
+rc_full() {
+	echo `realpath $BASH_SOURCE`
+}
+
+rc_path() {
+	echo $(dirname `rc_full`)
+}
+
+rc_name() {
+	echo $(basename `rc_full`)
+}
+
 bookmark() {
-        typeCheck=$(type $1)
+        typeCheck=$(type $1 &> /dev/null)
         if [ $? -eq 0 ]; then
                 echo "Name collision.  Can't make bookmark."
                 echo $typeCheck
@@ -22,16 +23,30 @@ bookmark() {
         fi
         makeAlias="alias $1='cd $(pwd)'"
         echo $makeAlias
-	echo ${path}/bookmarks
-        echo $makeAlias >> ${path}/bookmarks
+        echo $makeAlias >> `rc_path`/bookmarks
+	echo "Saved to `rc_path`/bookmarks"
         eval ${makeAlias}
 }
-source ${path}/bookmarks
+source `rc_path`/bookmarks
 
 #Setups
-cat ~/.vimrc | grep "source ${path}/vimrc.vim" &> /dev/null
+cat ~/.vimrc | grep "source `rc_path`/vimrc.vim" &> /dev/null
 if [ $? -ne 0 ]; then
-	echo "source ${path}/vimrc.vim" >> ~/.vimrc
+	echo "source `rc_path`/vimrc.vim" >> ~/.vimrc
 fi
 
-unset self path name
+#Aliases
+alias commonrc="vim `rc_full`"
+alias refresh="source `rc_full`"
+alias bookmarks="vim `rc_path`/bookmarks"
+alias vimrc="source `rc_path`/vimrc.vim"
+
+#Exports
+export HISTSIZE=$hist_size
+
+#Sources
+source `rc_path`/cernrc.sh
+
+
+#Cleanup local
+unset hist_size
