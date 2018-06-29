6cb329269f5e2ff6c3255f084d2e6941c7ac38eb
EgMan <antonio95woods@gmail.com>
2018-06-29 17:06:49 -0500

    Added a nifty little loading bar while commonrc runs

--- commonrc.sh
+++ commonrc.sh
@@ -1,7 +1,59 @@
 #Local
 hist_size=1000000
+loading_bar_size=7
+loading_bar_current=1
+
+#Functions
+clear_loading_bar(){
+    for i in `seq $loading_bar_size`; do
+        echo -ne '\b'
+    done
+}
+render_loading_bar(){
+    if [ $loading_bar_current -ne 0 ]; then
+        clear_loading_bar
+    fi
+    for i in `seq $loading_bar_size`; do
+        if [ $i -le $loading_bar_current ];then
+            echo -ne '█'
+        else
+            echo -ne '░'
+        fi
+    done
+    loading_bar_current=$((loading_bar_current + 1))
+}; render_loading_bar
+rc_full() {
+    #ret= `realpath $BASH_SOURCE`
+    #[ -z "$ret" ] && ret=${0:a}
+    #echo $ret
+
+    #TODO FIX THIS HARDCODING
+    echo "/home/aw055790/.rc/commonrc.sh"
+}
+rc_path() {
+    echo $(dirname `rc_full`)
+}
+rc_name() {
+    echo $(basename `rc_full`)
+}
+#Todo: accept optional argument to save to alternate file
+#Todo: save files to subdirectory
+bookmark() {
+        typeCheck=$(type $1 2>&1)
+        if [ $? -eq 0 ]; then
+                echo "Name collision.  Can't make bookmark."
+                echo $typeCheck
+                return
+        fi
+        makeAlias="alias $1='cd $(pwd)'"
+        echo $makeAlias
+        echo $makeAlias >> `rc_path`/bookmarks
+        echo "Saved to `rc_path`/bookmarks"
+        eval ${makeAlias}
+}
 
 #Tmux
+render_loading_bar
 if [ -z $TMUX ]; then
     if [[ "$TERM" == "screen" ]]; then
         echo "Screen is garbage.  Use tmux instead."
@@ -19,39 +71,8 @@ if [ -z $TMUX ]; then
     fi
 fi
 
-#Functions
-rc_full() {
-    #ret= `realpath $BASH_SOURCE`
-    #[ -z "$ret" ] && ret=${0:a}
-    #echo $ret
-
-    #TODO FIX THIS HARDCODING
-    echo "/home/aw055790/.rc/commonrc.sh"
-}
-rc_path() {
-    echo $(dirname `rc_full`)
-}
-rc_name() {
-    echo $(basename `rc_full`)
-}
-
-#Todo: accept optional argument to save to alternate file
-#Todo: save files to subdirectory
-bookmark() {
-        typeCheck=$(type $1 2>&1)
-        if [ $? -eq 0 ]; then
-                echo "Name collision.  Can't make bookmark."
-                echo $typeCheck
-                return
-        fi
-        makeAlias="alias $1='cd $(pwd)'"
-        echo $makeAlias
-        echo $makeAlias >> `rc_path`/bookmarks
-        echo "Saved to `rc_path`/bookmarks"
-        eval ${makeAlias}
-}
-
 #Argument parsing
+render_loading_bar
 force_setup=1
 while [[ $# -gt 0 ]]; do
     case "$1" in
@@ -64,6 +85,7 @@ while [[ $# -gt 0 ]]; do
 done
 
 #One time setup
+render_loading_bar
 if [ ! -f `rc_path`/.deleteme_to_rerun_setup ] || [ $force_setup -eq 1 ];then
     [ ! -f ~/.vimrc ] && touch ~/.vimrc
     cat ~/.vimrc | grep "source `rc_path`/vimrc.vim" &> /dev/null
@@ -87,6 +109,7 @@ if [ ! -f `rc_path`/.deleteme_to_rerun_setup ] || [ $force_setup -eq 1 ];then
 fi
 
 #Aliases
+render_loading_bar
 alias commonrc="vim `rc_full`"
 alias refresh="source `rc_full`"
 alias bookmarks="vim `rc_path`/bookmarks"
@@ -100,10 +123,14 @@ alias nahmsayin="vim `rc_path`/nahmsayin_prompt.zsh-theme"
 export HISTSIZE=$hist_size
 
 #Sources
+render_loading_bar
 source `rc_path`/cernrc.sh
 source `rc_path`/wslrc.sh
 source `rc_path`/bookmarks
 
 #Cleanup local
+render_loading_bar
 unset hist_size
 unset force_setup
+unset loading_bar_current
+clear_loading_bar
