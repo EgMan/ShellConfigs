787374f3ba34f14c2c8df7651a58057e7a765492
Antonio Woods <antonio95woods@gmail.com>
2018-10-25 15:01:25 -0500

    Add powershell aliases

--- wslrc.sh
+++ wslrc.sh
@@ -1,6 +1,15 @@
 alias start='cmd.exe /c start'
+alias powershell='powershell.exe'
+alias ejectcd="power 'powershell (New-Object -com 'WMPlayer.OCX.7').cdromcollection.item(0).eject()' >/dev/null &"
+alias controlpanel="power 'control panel' >/dev/null"
+alias abortallshutdown='power "while(1){shutdown -a *>\$null; if(\$lastexitcode -eq 0){\$time = get-date -Format HH:mm:ss ; echo \"\`nSuccesfully aborted a shutdown (\$time)\"}}"'
 export DISPLAY=:0
 opendir() {
     `cd $1; start .`
+}
+power(){
+    powershell.exe <<done
+$@
+done
 }
 
