ec67829e0b13a6ce06591a01c7f5e56cb70c131e
Antonio Woods <antonio95woods@gmail.com>
2019-06-14 11:27:24 -0500

    Add function to easily save unstaged git changes to reflog

--- wslrc.sh
+++ wslrc.sh
@@ -1,3 +1,4 @@
+alias wsl='wsl.exe'
 alias start='cmd.exe /c start'
 alias powershell='powershell.exe'
 alias ejectcd="power 'powershell (New-Object -com 'WMPlayer.OCX.7').cdromcollection.item(0).eject()' >/dev/null &"
@@ -5,6 +6,7 @@ alias controlpanel="power 'control panel' >/dev/null"
 alias abortallshutdown='power "while(1){shutdown -a *>\$null; if(\$lastexitcode -eq 0){\$time = get-date -Format HH:mm:ss ; echo \"\`nSuccesfully aborted a shutdown (\$time)\"}}"'
 alias chrome='"/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe"'
 alias origin='chrome `git config --get remote.origin.url`'
+alias subl='"/mnt/c/Program Files/Sublime Text 3/sublime_text.exe"'
 export DISPLAY=:0
 opendir() {
     `cd $1; start .`
